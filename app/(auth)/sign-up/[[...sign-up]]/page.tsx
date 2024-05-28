88adf2a864674fe3bc75962f90e985a25d7dd141
Aakash <aakashpc123@gmail.com>
2024-05-29 00:38:23 +0530

    ui/ux of login and signup

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,31 @@
-const SignUp = () => {
-    return ( 
-        <div>
-            signup page
+import { Loader2 } from "lucide-react";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-gray-800">
+            Join Us Today!
+          </h1>
+          <p className="text-base text-[#7E8CA0]">
+            Sign up to create an account and start your journey!
+          </p>
         </div>
-     );
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full box-shadow-md bg-[#7dfca393] hidden lg:flex items-center justify-center">
+        <Image src="/logo6.svg" height={100} width={700} alt="logo" className="object-contain" />
+      </div>
+    </div>
+  );
 }
- 
-export default SignUp;
---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher
+([
+    "/",
+
+]);
+
+
+
+export default clerkMiddleware((auth, request)=>{
+    if(isProtectedRoute(request)){
+        auth().protect();
+    }
+    return NextResponse.next()
+
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", 
+   "/", "/(api|trpc)(.*)"],
+};
