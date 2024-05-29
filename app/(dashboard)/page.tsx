16e87db037e4f5389abaf334572ee864f796c6dc
Aakash <aakashpc123@gmail.com>
2024-05-29 10:52:41 +0530

    login updation

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,13 @@
-import { Button  } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <p>
         THis is an authenticated route
+        <UserButton 
+          afterSignOutUrl="/"
+          /> 
       </p>
     </div>
   );
