3587830421756bc6645561daa8b37576a55d6eff
aakash10802 <aakashpc123@gmail.com>
2024-06-01 22:12:14 +0530

    second commit

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,12 +1,42 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-
+import {z} from "zod";
+import { zValidator } from '@hono/zod-validator'
 
 export const runtime ="edge";
 const app =new Hono().basePath('/api')
-app.get('/hello',(c)=>{
+app
+
+.get(
+    "/hello",
+    (c)=>{
     return c.json({
         message:"hello Next.js"
+    })
+})
+.get(
+    "/hello/:test",
+    (c)=>{
+    return c.json({
+        message:"hello world"
+    })
+})
+
+.post(
+    "/create/:postId",
+zValidator("json",z.object({
+    name:z.string(),
+    userId:z.string(),
+})),
+zValidator("param",z.object({
+    postId: z.number(),
+})),
+(c)=>{
+
+    const {name, userId} = c.req.valid("json");
+    const {postId} = c.req.valid("param");
+    return c.json({
+         
     })
 })
 
--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher
 ([
     "/",
-
+ 
 ]);
 
 
