5e9168a877b407ae4d904b0eec099c1046ec85d4
aakash10802 <aakashpc123@gmail.com>
2024-06-08 02:46:54 +0530

    changes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,17 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-
+import  accounts  from "./accounts";
 
 export const runtime ="edge";
 
 
 const app = new Hono ().basePath("/api");
-
-app.get("/hello",
-(c)=>{
-    return c.json({hello:" World!"});
-});
-
+app.route("/accounts",accounts)
 
 export const GET = handle(app);
 export const POST = handle(app);
