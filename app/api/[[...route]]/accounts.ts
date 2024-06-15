22befdc1f7e716fb4d56fd8f3fb814c3d6c0e774
aakash10802 <aakashpc123@gmail.com>
2024-06-16 00:24:54 +0530

    new chapter

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
+import { eq } from "drizzle-orm";
 import { clerkMiddleware,getAuth } from "@hono/clerk-auth";
-
+import{HTTPException} from  "hono/http-exception"
 import { db } from "@/db/dirzzle";
 import { accounts } from "@/db/schema";
 
@@ -10,16 +11,20 @@ const app = new Hono().get(
     clerkMiddleware(),
     async(c)=>{
     const auth =getAuth(c)
+    
     if(!auth?.userId){
-        return c.json({
-            error:"unauthorized"
-        },401);
+        // return c.json({error: "Authorized"},401);
+        throw new HTTPException(401,{
+            res: c.json({error: "unAuthorized"},401),
+        })
     }
-     const data =await db.select({
+     const data =await db
+     .select({
         id: accounts.id,
         name: accounts.name,
      })
-     .from(accounts);
+     .from(accounts)
+     .where(eq(accounts.userId, auth.userId));
      
     
     return c.json({data});
