a50c34d6db6bd1e437e69b55a4ae3734ea8995a7
aakash10802 <aakashpc123@gmail.com>
2024-06-29 11:01:37 +0530

    loader

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,49 +1,67 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
-
 import {
     Card,
     CardContent,
     CardTitle,
     CardHeader
-
-} from "@/components/ui/card"
-import { Plus } from "lucide-react";
+} from "@/components/ui/card";
+import { Loader2, Plus } from "lucide-react";
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
-
-const AccountPage =()=>{
-
+const AccountPage = () => {
     const newAccount = useNewAccount();
-    return(
-        <div className="max-w-screen-2xl mx-auto  w-full pb-10 -mt-24 ">
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+   
+    if (accountsQuery.isLoading){
+        return(
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader>
+                        <Skeleton className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin"/>
+                        </div>
+                    </CardContent>
+                </Card>
+
+            </div>
+        )
+    }
+    
+
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
                 <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
                     <CardTitle className="text-xl line-clamp-1">
                         Account Page
                     </CardTitle>
-                 <Button onClick={newAccount.onOpen} size="sm">
-                    <Plus className="size-4 m-2"/>
-                    Add New
-                 </Button>
-
+                    <Button onClick={newAccount.onOpen} size="sm">
+                        <Plus className="size-4 m-2" />
+                        Add New
+                    </Button>
                 </CardHeader>
                 <CardContent>
                     <DataTable 
-                    filterKey="email" 
-                    columns={columns} 
-                    data={[]}
-                    onDelete={()=>{}}
-                    disabled={false}
+                        filterKey="email" 
+                        columns={columns} 
+                        data={accounts}
+                        onDelete={() => {}} 
+                        disabled={false} 
                     />
                 </CardContent>
-
-
             </Card>
         </div>
-    )
-}
+    );
+};
+
 export default AccountPage;
